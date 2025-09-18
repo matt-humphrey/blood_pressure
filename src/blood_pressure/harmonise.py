3c3763d8d05ca880cbbc494d94babaf2368002d0
Matt Humphrey <34499789+matt-humphrey@users.noreply.github.com>
2025-09-18 10:14:45 +1000

    add function to convert G126_SLPT time from AM to PM

--- src/blood_pressure/harmonise.py
+++ src/blood_pressure/harmonise.py
@@ -11,14 +11,46 @@ def sort_by_id(df: DataFrame) -> DataFrame:
 def replace_missing_values(df: DataFrame) -> DataFrame:
     """Replace values for each given column that..."""
     return df.with_columns(
-        # pl.col(r"^G\w{3}_DM\d{1,2}[A-Z]$").replace({-99: None, -88: None}),
+        pl.col(r"^G\w{3}_BP\d+$").replace({-99: None, -88: None, 999: None}),
+        pl.col(r"^G\w{3}_BP[DS][1-6]$").replace({-99: None, -88: None}),
+        pl.col(r"^G\w{3}_SHR[1-6]$").replace({-99: None, -88: None}),
     )
 
 
 def recast_types(df: DataFrame) -> DataFrame:
     """Recast column types as new type"""
     return df.with_columns(
-        # pl.col(r"^G\w{3}_DM\d{1,2}[A-E]$").cast(pl.Int64),
+        pl.col(r"^G\w{3}_BP\d+$").cast(pl.Int64),
+        pl.col(r"^G\w{3}_BP[DS][1-6]$").cast(pl.Int64),
+        pl.col(r"^G\w{3}_SHR[1-6]$").cast(pl.Int64),
+    )
+
+
+def apply_rounding(df: DataFrame) -> DataFrame:
+    """Recast column types as new type"""
+    return df.with_columns(
+        pl.col(r"^G\w{3}_PWC170$").cast(pl.Float64).round(2),
+    )
+
+
+def recode_bp41(df: DataFrame) -> DataFrame:
+    """Replace errant value of 1110 with None"""
+    return df.with_columns(pl.col("G214_BP41").replace({1110: None}))
+
+
+def update_time_for_g126_slpt(df: DataFrame) -> DataFrame:
+    """
+    Convert times from AM to PM for `G126_SLPT`.
+
+    This function converts the time column to a datetime object, because Polars can't perform
+    arithmetic on time objects, and adds 12 hours, before converting back to a time object.
+    """
+    return df.with_columns(
+        pl.datetime(2000, 1, 1)  # add arbitary date to enable arithmetic on datetime object
+        .dt.combine(pl.col("G126_SLPT"))
+        .dt.offset_by("12h")
+        .dt.time()
+        .alias("G126_SLPT")
     )
 
 
@@ -26,10 +58,10 @@ initial_transforms = [replace_missing_values]
 final_transforms = [recast_types, sort_by_id]
 
 dataset_transforms = {
-    "G126": [],
+    "G126": [update_time_for_g126_slpt],
     "G208": [],
-    "G214": [],
-    "G217": [],
+    "G214": [apply_rounding, recode_bp41],
+    "G217": [apply_rounding],
     "G222": [],
 }
 
