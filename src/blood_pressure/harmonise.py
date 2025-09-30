5045f2ee9af7b22bf3a9f873bc20797e43ac0c94
Matt Humphrey <34499789+matt-humphrey@users.noreply.github.com>
2025-09-30 16:51:53 +0800

    harmonise G126_BPSL, G222_BP_TIME and G222_WKT

--- src/blood_pressure/harmonise.py
+++ src/blood_pressure/harmonise.py
@@ -4,6 +4,8 @@ from typing import Callable
 import polars as pl
 from polars import DataFrame
 
+from blood_pressure.config import RAW_DATA
+
 __all__ = ["DATA_TRANSFORMS"]
 
 
@@ -78,7 +80,20 @@ def offset_time(col: str, offset: str = "12h") -> pl.Expr:
     )
 
 
-def update_time_for_g126_slpt(df: DataFrame) -> DataFrame:
+def round_time(col: str, by: str = "1m") -> pl.Expr:
+    """
+    Create a Polars Expression to offset a column with dtype Time by a given amount
+    """
+    return (
+        pl.datetime(2000, 1, 1)  # add arbitary date to enable arithmetic on datetime object
+        .dt.combine(pl.col(col))
+        .dt.round(by)
+        .dt.time()
+        .alias(col)
+    )
+
+
+def update_g126_slpt(df: DataFrame) -> DataFrame:
     """
     Convert times from AM to PM for `G126_SLPT`.
 
@@ -88,21 +103,47 @@ def update_time_for_g126_slpt(df: DataFrame) -> DataFrame:
     return df.with_columns(offset_time("G126_SLPT", "12h"))
 
 
-def update_time_for_g126_bpsl(df: DataFrame) -> DataFrame:
+def update_g222_bp_time(df: DataFrame) -> DataFrame:
     """
-    Convert mislabelled times from AM to PM
+    The raw data for many of the values for G222_BP_TIME ended in 59 seconds (ie. 17:50:59).
+    This data must have been changed somehow, because data from FileMaker had no seconds value.
+    Furthermore, a number of values from FileMaker didn't match what was in core, primarily cases
+    where the value was offset by 12 hours (where it was captured as PM, but should have been AM).
 
-    Only one notable instance for ID 4801 (10:16 -> 22:16)
+    This function reads the data pulled out of FileMaker, formats it, merges with what's in core,
+    and then corrects values from it that are suspect and have been captured as AM but should be PM.
+    (Data from FileMaker has correctly converted many values from PM to AM, but also "corrected"
+    values which should have remained as PM, and hence are being converted back).
+
+    There are values that range between 9am and 5pm, which are considered unusual, given that this
+    was a sleep study, and most blood pressures were captured in the evening, from 6pm onwards.
+    For all values for `G222_BP_TIME` before 12pm, if there was data for various sleep variables
+    captured, convert to PM, and otherwise keep as is - some assessments were done in the morning
+    (or afternoon) if the participant did not partake in the sleep study.
     """
-    return df.with_columns(
-        pl.when(pl.col("G126_BPSL").is_between(time(1), time(12)))
-        .then(offset_time("G126_BPSL", "12h"))
-        .otherwise(pl.col("G126_BPSL"))
-        .alias("G126_BPSL")
+    updated_bp_time = pl.read_csv(
+        RAW_DATA / "BPTIME.csv",
+        has_header=False,
+        new_columns=["ID", "G222_BP_TIME"],
+        schema={"ID": pl.Utf8, "G222_BP_TIME": pl.Time},
+    )
+    cleaned_updated_bp_time = updated_bp_time.with_columns(
+        pl.col("ID").str.strip_chars_end().cast(pl.Float64)
+    )
+    df_updated = df.update(cleaned_updated_bp_time, on="ID")
+
+    return df_updated.with_columns(
+        pl.when(
+            pl.col("G222_BP_TIME").lt(time(12))
+            & ~pl.all_horizontal(pl.col(r"^G222_(BPSL|SLPT|WKT|WKP|DNWN_PSG)$").is_null())
+        )
+        .then(offset_time("G222_BP_TIME"))
+        .otherwise(pl.col("G222_BP_TIME"))
+        .alias("G222_BP_TIME")
     )
 
 
-def update_time_for_g222_slpt(df: DataFrame) -> DataFrame:
+def update_g222_slpt(df: DataFrame) -> DataFrame:
     """
     Convert mislabelled times from AM to PM
     """
@@ -130,15 +171,92 @@ def clean_string_column(col: str) -> Callable:
     return preprocessor
 
 
+def update_g126_bpsl(df: DataFrame) -> DataFrame:
+    """
+    Update the values of `G126_BPSL` for instances where the value was incorrectly entered.
+    """
+    new_g126_bpsl = {
+        "ID": [
+            802,
+            1601,
+            3701,
+            4801,
+            55801,
+            64101,
+            67201,
+            68502,
+            87401,
+            93301,
+            94702,
+            95501,
+            104502,
+            114802,
+            157701,
+            186101,
+            200902,
+            207901,
+            215701,
+            225002,
+        ],
+        "G126_BPSL": [
+            time(21, 45),
+            time(22),
+            time(21, 35),
+            time(22, 16),
+            time(22, 5),
+            time(22),
+            time(22),
+            time(22, 35),
+            time(21, 1),
+            time(21, 30),
+            time(21, 23),
+            time(20, 45),
+            time(21),
+            time(21),
+            time(22, 19),
+            time(22, 19),
+            time(21, 35),
+            time(22, 6),
+            time(21, 58),
+            time(23, 15),
+        ],
+    }
+
+    df_new_g126_bpsl = pl.DataFrame(new_g126_bpsl, schema={"ID": pl.Float64, "G126_BPSL": pl.Time})
+    return df.update(df_new_g126_bpsl, on="ID")
+
+
+def update_g222_wkt(df: DataFrame) -> DataFrame:
+    """
+    Update the values of `G222_WKT` for instances where the value was incorrectly entered.
+    """
+    new_g222_wkt = {
+        "ID": [15900, 17130, 17830, 41310, 41730, 44100],
+        "G222_WKT": [time(5, 45), time(6, 25), time(6, 15), time(6), time(5, 47), time(6, 15)],
+    }
+
+    df_new_g222_wkt = pl.DataFrame(new_g222_wkt, schema={"ID": pl.Float64, "G222_WKT": pl.Time})
+    return df.update(df_new_g222_wkt, on="ID")
+
+
 initial_transforms = [replace_missing_values]
 final_transforms = [recast_types, sort_by_id]
 
 dataset_transforms = {
-    "G126": [update_time_for_g126_slpt, clean_string_column("G126_SL_COM")],
+    "G126": [
+        update_g126_slpt,
+        update_g126_bpsl,
+        clean_string_column("G126_SL_COM"),
+    ],
     "G208": [],
     "G214": [apply_rounding, recode_bp41, recode_xcar_g214],
     "G217": [apply_rounding, recode_xcar_g217],
-    "G222": [update_time_for_g222_slpt, clean_string_column("G222_SL_COM")],
+    "G222": [
+        update_g222_slpt,
+        update_g222_bp_time,
+        update_g222_wkt,
+        clean_string_column("G222_SL_COM"),
+    ],
 }
 
 DATA_TRANSFORMS = {
